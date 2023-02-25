d22825b0cda4692facb97a524cafc6bb6b07fffa
marcinhojazz <marcinhojazz@gmail.com>
2023-02-25 11:04:15 -0300

    updates and add reflections and maps textures

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,51 @@
-import { Canvas, useFrame } from '@react-three/fiber';
+import React, { useEffect, useRef } from 'react';
+import { Canvas, useFrame, useLoader } from '@react-three/fiber';
 import { MeshReflectorMaterial } from '@react-three/drei';
-import { useRef } from 'react';
-import { Mesh, MeshStandardMaterial, PerspectiveCamera } from 'three';
+import { LinearEncoding, Mesh, MeshStandardMaterial, PerspectiveCamera, RepeatWrapping, TextureLoader } from 'three';
+import { Ground } from './components/Ground';
 
 function Box() {
   const boxRef = useRef<Mesh>(null!);
 
+  const [roughness, normal] = useLoader(TextureLoader, [
+    'textures/terrain-normal.jpg',
+    'textures/terrain-roughness.jpg',
+  ]);
+
+  useEffect(() => {
+    [normal, roughness].forEach((t) => {
+      t.wrapS = RepeatWrapping;
+      t.wrapT = RepeatWrapping;
+      t.repeat.set(5, 5);
+      t.offset.set(0, 0);
+    });
+    normal.encoding = LinearEncoding;
+  }, [normal, roughness]);
+  
+  useFrame((state, delta) => {
+    let t = -state.clock.getElapsedTime() * 0.128;
+    roughness.offset.set(0, t % 1);
+    normal.offset.set(0, t % 1);
+  });
+
   useFrame(() => {
-    boxRef.current.rotation.x += 0.005;
-    boxRef.current.rotation.y += 0.01;
+    boxRef.current.rotation.x += 0.01;
+    boxRef.current.rotation.y += 0.02;
   })
   
   return (
     <mesh ref={boxRef} castShadow receiveShadow>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color="crimson" />
+      <boxGeometry args={[2, 2, 2]} />
+      <meshStandardMaterial 
+        color="crimson" 
+        />
       <MeshReflectorMaterial 
+        normalMap={normal}
+        normalScale={[0.15, 0.15]}
+        roughnessMap={roughness}
         envMapIntensity={0}
         dithering={true}
         color={[0.7, 0.015, 0.015]}
-        roughness={0.7}
-        blur={[1000, 400]}
-        mixBlur={30}
-        mixStrength={80}
-        mixContrast={1}
-        resolution={1024}
-        mirror={0} 
-        depthScale={0.01}
-        minDepthThreshold={0.9}
-        maxDepthThreshold={1}
-        depthToBlurRatioBias={0.25}
-        // debug={0}
-        reflectorOffset={0.2}
-      />
-    </mesh>
-  )
-}
-
-function Ground() {
-  return (
-    <mesh rotation-x={-Math.PI * 0.5} castShadow receiveShadow>
-      <planeGeometry args={[30, 30]} />
-      <MeshReflectorMaterial 
-        envMapIntensity={0}
-        dithering={true}
-        color={[0.015, 0.015, 0.015]}
         roughness={0.7}
         blur={[1000, 400]}
         mixBlur={30}
